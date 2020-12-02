5266c24a72e508a4da3da9efb2ad7ac566eb3fa8
MichaelHazielAmbrocioPerez <71995114+MichaelHazielAmbrocioPerez@users.noreply.github.com>
2020-12-01 18:30:20 -0600

    Create Nodo.java

---
+++ U3E5/Nodo.java
@@ -0,0 +1,11 @@
+package u3e5;
+public class Nodo {
+
+    int elemento;
+    Nodo Siguiente;
+    
+    public Nodo (int elemento){
+        this.elemento=elemento;
+        Siguiente=null; 
+}    
+}
